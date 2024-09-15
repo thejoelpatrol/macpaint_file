1a306e78841c1d4877e42c723a26a83fcc7cd7ad
Joel Cretan <jcretan@gmail.com>
2024-09-15 02:19:36 -0400

    convert to MacPaint

--- converter.py
+++ converter.py
@@ -2,12 +2,20 @@ import argparse
 from macpaint import MacPaintFile
 
 def main(args):
-    macpaint_file = MacPaintFile.from_file(args.infile)
-    macpaint_file.to_png(args.outfile)
+    if args.from_macpaint:
+        macpaint_file = MacPaintFile.from_file(args.infile)
+        macpaint_file.to_png(args.outfile)
+    elif args.to_macpaint:
+        macpaint_file = MacPaintFile.from_png(args.infile)
+        macpaint_file.write_file(args.outfile)
+    else:
+        raise RuntimeError("must specify either --from-macpaint or --to-macpaint")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("infile", help="MacPaint file path")
-    parser.add_argument("outfile", help="Output png path")
+    parser.add_argument("--from-macpaint", "-m", action="store_true", help="Convert from MacPaint to PNG")
+    parser.add_argument("--to-macpaint", "-p", action="store_true", help="Convert from PNG to MacPaint")
+    parser.add_argument("infile", help="Input file path")
+    parser.add_argument("outfile", help="Output file path")
     args = parser.parse_args()
     main(args)
